040f63752ff7b4fc0929be8382b08e6859490127
CristopherWallau <criswallau2303@gmail.com>
2026-03-10 17:43:27 -0300

    !

--- firstclass.cpp
+++ firstclass.cpp
@@ -1,7 +1,22 @@
-#include stdio.h
-#include stdlib.h
-#include string.h
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int N, K, D;
+    cin >> N >> K >> D;
+    vector<int> flag_city[K];
+    for(int i = 0; i < K; i++) {
+        int value;
+        cin >> value;
+        if(flag_city[value-1].back == 1) {
+            continue;
+        }
+        flag_city[value-1].push_back(1);
+    }
+    vector<int> adj[N];
+    for(int i = 0; i < N-1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u-1].push_back(v-1);
+        adj[v-1].push_back(u-1);
+    }
 }
